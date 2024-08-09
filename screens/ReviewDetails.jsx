544279b837edcd9c9b93886dea102c95bc6999b1
ALAM <alam.2008@live.co.uk>
2024-08-09 12:38:42 +0100

    first commit

---
+++ screens/ReviewDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ReviewDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>Review Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+  },
+});
