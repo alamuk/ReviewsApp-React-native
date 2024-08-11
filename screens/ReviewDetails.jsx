c2f13b17614e703c886509e71795f2db386a3675
ALAM <alam.2008@live.co.uk>
2024-08-11 17:43:18 +0100

    Rating images for review page

--- screens/ReviewDetails.jsx
+++ screens/ReviewDetails.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import { globalStyles } from '../Styles/global';
 import Card from '../shared/Card';
+import { images } from '../Styles/global';
 
 export default function ReviewDetails({ route }) {
-  // console.log(route);
-
   const item = route.params;
+  const rating = item.rating;
+
   return (
     <View style={globalStyles.container}>
       <Card>
-        <Text style={globalStyles.titleText}> {item.title}</Text>
-        <Text style={globalStyles.paragraph}> {item.body}</Text>
-        <Text style={globalStyles.paragraph}> {item.rating}</Text>
+        <Text> {item.title}</Text>
+        <Text> {item.body}</Text>
+        <View style={styles.rating}>
+          <Text> GameZone Rating:</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
+});
